ce7e67c4fd228c23da2e2f2374a008e2195c0650
Alex Angelini <alex.louis.angelini@gmail.com>
2024-03-07 15:39:38 +0100

    Rate limit .local hostnames

--- main.lua
+++ main.lua
@@ -1,13 +1,58 @@
 local ngx = ngx
 local http = require("resty.http")
+local cjson = require("cjson.safe")
 
 local _M = {}
 
+function string:endswith(suffix)
+  return self:sub(-#suffix) == suffix
+end
+
+function remove_port(host)
+  local port_idx = string.find(host, ":")
+  if port_idx ~= nil then
+    host = string.sub(host, 0, port_idx-1)
+  end
+  return host
+end
+
+function is_ratelimited_host(host)
+  return host:endswith("local")
+end
+
+function rate_for_host(host)
+  -- TODO: change the rate based on the hostname
+  return 5
+end
+
 function _M.rewrite()
   local httpc = http.new()
+
+  local host = remove_port(ngx.req.get_headers()["Host"])
+  if not is_ratelimited_host(host) then
+    return
+  end
+
+  local key = "host:" .. host
+
+  local body = {
+    requests={
+      {
+        name="requests_per_sec",
+        unique_key=key,
+        hits=1,
+        limit=rate_for_host(host),
+        duration=10000,
+        algorithm=1
+      }
+    }
+  }
+
+  local body_json = cjson.encode(body)
+
   local res, err = httpc:request_uri("http://gubernator.gubernator.svc.cluster.local/v1/GetRateLimits", {
     method = "POST",
-    body = '{"requests":[{"name":"requests_per_sec","unique_key":"example:1","hits":1,"limit":5,"duration":10000,"algorithm":1}]}',
+    body = body_json,
     headers = {
         ["Content-Type"] = "application/json",
     },
@@ -17,8 +62,13 @@ function _M.rewrite()
       return
   end
 
-  ngx.log(ngx.ERR, "request succeeded: ", res.status)
-  ngx.log(ngx.ERR, "body: ", res.body)
+  local res_body = cjson.decode(res.body)
+  local status = res_body.responses[1].status
+  if status == "OVER_LIMIT" then
+    ngx.stauts = 429
+    ngx.log(ngx.ERR, "Host over request limit: ", host)
+    ngx.exit(ngx.HTTP_FORBIDDEN)
+  end
 end
 
 return _M
